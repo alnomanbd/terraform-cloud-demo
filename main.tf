e698be8d42bf7d128683bbde14b94f5a40722340
alnomanbd <toalnoman@gmail.com>
2025-05-21 14:49:38 +0600

    some modification done

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ variable "secret_key" {}
 variable "subnet_id" {}
 
 resource "aws_instance" "terraform-cloud-demo" {
-    ami           = var.ami,
-    instance_type = var.instance_type,
-    region        = var.region,
-    subnet_id     = var.subnet_id,
+    ami           = var.ami
+    instance_type = var.instance_type
+    region        = var.region
+    subnet_id     = var.subnet_id
     tags = {
         Name = "Terraform Cloud Demo"
     }
